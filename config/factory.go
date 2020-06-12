73dfcf2861bfc1843b740810ec97d73ed0a42507
TABRIZ ATAYI <tebrizetayi@gmail.com>
2020-06-12 12:47:49 +0200

    Test Database Configuration is added.

--- config/factory.go
+++ config/factory.go
@@ -14,12 +14,24 @@ var Cnf = &Config{
 	IsDevelopment: true,
 }
 
-/*host     = "localhost"
-port     = 5555
-user     = "postgres"
-password = "postgres"
-dbname   = "testdb"*/
+var testCnf = &Config{
+	Database: DatabaseConfig{
+		Type:         "postgres",
+		Host:         "localhost",
+		Port:         5432,
+		User:         "postgres",
+		Password:     "",
+		DatabaseName: "go_todolist_server_test",
+		MaxIdleConns: 5,
+		MaxOpenConns: 5,
+	},
+	IsDevelopment: true,
+}
 
 func NewConfig() *Config {
 	return Cnf
+}
+
+func NewTestConfig() *Config {
+	return testCnf
 }
